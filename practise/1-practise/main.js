dcf6ef2b49f476b8b39024b31049ebd16f24a359
abduqodir31 <agafurjonov6@gmail.com>
2022-12-10 22:28:15 +0500

    salam

---
+++ practise/1-practise/main.js
@@ -0,0 +1,104 @@
+// let bus = prompt ("butun soni berin ")                                                        (1)
+// if ( bus > 0 ){
+//    bus++;
+//    console.log(bus);
+// }
+
+// let car = prompt ("butun soni berin ")                                                    (2)
+// if ( car > 0 ){
+//    car++;
+//    console.log(car);
+// }
+// else if (car < 0 ){
+//    car-=2;
+//    console.log(car);
+// }
+
+// let stop = prompt ("butun soni berin ")                                                             (3)
+// if (stop > 0){
+//    stop++;
+//    console.log(stop);
+// }
+// else if ( stop < 0){
+//    stop-=2;
+//    console.log(stop);
+// }
+// else if ( stop = 10){
+//    console.log(stop);
+// }
+
+//    let  a = +prompt("butun soni kiritin")                                                                        ( 4)
+//    let b = ('')
+//    let c = ('')
+//    if (a){
+//       console.log(a);
+//    }
+
+//    else if ( b > 0){
+//        console.log(b);
+//    }
+
+//    else if ( c > 0){
+//       console.log(c);
+//   }
+
+// let s = +prompt("butun soni kiriting")                                                                         (5)
+// let q = ('')
+// let r = ('')
+//  if ( s > 0){
+//    console.log(s);
+// }
+// else if ( q > 0){
+//   console.log(q);
+// }
+// else if ( r > 0){
+//    console.log(r);
+//  }
+//  else if ( s < 0){
+//    console.log(s);
+// }
+// else if ( q < 0){
+//   console.log(q);
+// }
+// else if ( r < 0){
+//    console.log(r);
+//  }
+
+
+// let u = prompt("buttun soni kiriting 1-kichkina-20-kotassi-40");                                            (6)-(7)-(8)-(9)
+// let m = "40";
+
+// if ( u < 40 && u > 20 ) {
+//   console.log("eng kottasi");
+// }
+// else if ( u < 20 && u > 1){
+//    console.log("eng kichkinasi");
+// }
+
+// let mac = prompt("buttu sonni kiritin 60-kotta-30-kichkina-10-zero-0 ")                                              (10)-(11)
+// let l = 30;
+// let i = 15;
+// if ( mac < 60 && mac >= 30){
+// console.log ('kotta')
+// }
+// else if ( mac < 30 && mac > 10){
+//    console.log("kichkina");
+// }
+// else if ( mac > 0){
+//    console.log( 0 );
+// }
+
+
+// let sut = +prompt ("buttun soni kiriting")                                                                               (12)-(13)
+// let sud = 50;
+// let sup = 25;
+// let suv = 10;
+// if ( sut < 50 && sut > 25 ){
+//    console.log( "Eng kottasi ");
+// }
+// else if ( sut < 25 && sut > 10){
+//    console.log("Ortancha");
+// }
+// else if ( sut < 10 && sut > 0){
+//    console.log("Eng kichkinasi");
+// }
