49919cf08dcc69e57b3bc080a3ca73aa5203290c
abduqodir31 <agafurjonov6@gmail.com>
2022-12-13 19:16:59 +0500

    oka

---
+++ 3-dars/main.js
@@ -0,0 +1,3 @@
+// let a = prompt("raqam kiriting");
+// let b = "";
+// a % 2 === 0 ? console.log("juft son") : console.log("tog' son");
---
+++ practise/2-practise/js/main.js
@@ -0,0 +1,18 @@
+//  let a = prompt("Son kiriting")
+//  if ( a % 2==0 ){
+//     console.log( a ** 2);
+
+//  }
+//  else if ( a % 2 != 0){
+
+//     console.log( a ** 3);
+//  }
+
+
+ let b = prompt ("son kiriting c")
+ let c = ("");
+ let d =("") ;
+ let l = ("");
+if ( c + d + l){
+   console.log(true );
+}
