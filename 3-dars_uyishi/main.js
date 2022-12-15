72cc7fb1f919157c82b98bf944f7767e2b57eaee
abduqodir31 <agafurjonov6@gmail.com>
2022-12-15 16:24:45 +0500

    pot

---
+++ 3-dars_uyishi/main.js
@@ -0,0 +1,31 @@
+// let n = 0                                                                                                   (1)
+// let k = 10 
+// for ( let n = 0; n < 15; n++){
+//     console.log(k);
+// }
+
+// let a = 15                                                                                               (2)
+// let b = 31
+// for ( let a = 15; a < b ; a++ ){
+//     console.log(a);
+// }
+
+// let a = 14                                                                                                 (3)
+// let b = 31
+// for ( let b = 30; b > a ; b-- ){
+//     console.log(b);
+// }
+
+
+// const a = parseInt(prompt("Ijobiy raqam kiriting: "));                                   ("ustozni birinchi vazifasi")
+
+//   a % 2===0 ? console.log("tub son emas "): console.log("juft  son emas"); 
+
+
+// let a = prompt("Son kiriting");                                  ("ustozni ikkinchi va ohirgisi vazifasi")
+//   a ** 2 ? console.log(a**2): console.log(""); 
+
+// let b = prompt("1kg kanfetning narxini kiriting")
+// for (let i = 1 ; i < 11; i++){
+//      console.log(`${i}kg narxi:${i*b}`);
+//  }
---
+++ 4-dars/main.js
@@ -0,0 +1,61 @@
+// let num = +prompt('raqam kiriting')
+
+// let a = Math.floor ( num / 10 )
+// let b = num % 10 ;
+// console.log( b * 10 + a);
+
+// let num = +prompt("raqam kiriting")
+// let a = 20;
+// let b = 30;
+// let c = 50;
+// if ( b > a && b < c || b < a && b > a ){
+//     console.log(b);
+// }
+// else if ( a > b && a < c || a < b && a > c){
+//     console.log(a);
+// }
+// else if ( c > b && c < a || c < a && c > b){
+//     console.log(c);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let result ;
+// let operator = prompt("21 asir kalkulatori( + , - , / , *,)")
+// let number1 = parseFloat(prompt("birinchi raqamni kiriting"))
+// let number2 = parseFloat(prompt("ikkinchi raqanmi kiriting"))
+ 
+
+// switch( operator){
+//     case '+':
+//         result = number1 + number2;
+//         console.log(`$(number1) + $(number2) = $(result)`);
+//         break
+//         case '-':
+//             result = number1 - number2;
+//             console.log(`$(number1) - $(number2) = $(result)`);
+//             break
+//             case '*':
+//                 result = number1 * number2;
+//                 console.log(`$(number1) + $(number2) = $(result)`);
+//                 break
+//                 case '/':
+//                     result = number1 / number2;
+//                     console.log(`$(number1) / $(number2) = $(result)`);
+//                     break
+
+//                     default:
+//                         console.log('invalid operator');
+// }
+
+
+
+
