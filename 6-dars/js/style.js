ac431a970cc0d1d124b34161d5ab2da884c92408
abduqodir31 <agafurjonov6@gmail.com>
2022-12-22 16:52:40 +0500

    lopka

---
+++ 6-dars/js/style.js
@@ -0,0 +1,54 @@
+// let a = 50;                            (1- dars _ amaliyot)
+// const lalala = (c , b)=>{
+// return b (c)
+// }
+// const italy = lalala ( a , (b) =>{
+//     return b * 5;
+// })
+// console.log(italy);     
+
+// let num = 1;
+// const increment = (num)=>{
+//     return (numa =num + 1)
+// }
+// console.log(num);
+// console.log(num + 1);
+// console.log(num +2 );
+// console.log(num + 3);
+
+
+// let a = +prompt("son kiriting")
+// function x (){
+//     return a * (-1)
+// }
+// console.log(x());
+
+
+
+
+
+// let num = +prompt("son kiriting");
+
+// const musbat = (num)=>{
+//     if ( num < 0 ){
+//         return (num = num * -1);
+//     }
+//     else {
+//         return "bu son musbat son "
+//     }
+// }
+
+
+
+// const manfiy =(num)=>{
+//     if ( num > 0 ){
+//         return(num = num * -1)
+//     }
+//     else {
+//         return "bu son manfiy son"
+//     }
+// };
+// console.log(musbat(num));
+// console.log(manfiy(num));
+
+
---
+++ 7-dars/js/style.js
@@ -0,0 +1,10 @@
+let b = prompt ('Ism')
+let c = prompt ('Familiya')
+ let a = prompt("Yosh")
+
+ const user = {
+    name : b,
+    lastName : c ,
+    age : a,
+ }
+ console.log(user);
